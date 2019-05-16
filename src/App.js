bf8114949048008d8e2189bf0ac2cc8c01748004
Jack <evgen.mikhalov@gmail.com>
2019-05-16 13:46:44 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
 import logo from './logo.svg';
+import RectDOM from 'react-dom';
+import Store from './store';
+import createStore from './helper';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const initialState = {count:0};
+function updateState(state, action){
+  switch(action.type){
+    case 'INCREMENT': return {count: state.count + action.amount};
+    case 'DECREMENT': return {count: state.count - action.amount};
+    default: return state;
+  }
+}
+
+const incrementAction = {type: 'INCREMENT', amount: 1}
+const decrementAction = {type: 'DECREMENT', amount: 1}
+
+const store = new Store(updateState, initialState);
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+  }
+  componentDidMount() {
+    store.subscribe(()=>this.forceUpdate());
+  }
+  increment() {
+    store.update(incrementAction);
+  }
+  decrement() {
+    store.update(decrementAction);
+  }
+  render() {
+    return (
+      <div className="counter">
+        <div className="count">{store.state.count}</div>
+        <div className="buttons">
+          <button className="decrement" onClick={this.decrement}>-</button>
+          <button className="increment" onClick={this.increment}>+</button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
